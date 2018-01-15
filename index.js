4b8fc355c0dfb57e5ba4f771f2ef429ac98888e3
sanober riaz <sanober@sanober.me>
2018-01-15 01:56:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var cat = [];
+  cat = kittens;
+  cat.push(name);
+  return cat;
+}
+
+function prependKitten(name){
+  return var cat = [name, ...kittens];
+  
+}
+
+function removeLastKitten(){
+  var cat = kittens;
+  cat.pop();
+  return cat;
+}
+
+function removeFirstKitten(){
+  return var cat = kittens.slice(1);
+}
